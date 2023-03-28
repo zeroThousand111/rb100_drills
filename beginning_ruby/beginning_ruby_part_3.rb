41386c45de5224926575616c7d219ed748d986c1
Graham Jarvis <zeroThousand111@proton.me>
2023-03-28 11:13:23 +0000

    add select method for odd numbers

--- beginning_ruby/beginning_ruby_part_3.rb
+++ beginning_ruby/beginning_ruby_part_3.rb
@@ -1,14 +1,20 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-index = 4
-loop do
-    puts array[index]
-    index +=1
-    break if index == array.size
+# index = 4
+# loop do
+#     puts array[index]
+#     index +=1
+#     break if index == array.size
+# end
+
+# array.each { |item| puts item if item >=5}
+
+# array.each do |item|
+#     puts item if item >= 5 
+# end
+
+new_array = array.select do |item|
+    item.odd?
 end
 
-array.each { |item| puts item if item >=5}
-
-array.each do |item|
-    puts item if item >= 5 
-end
+p new_array
