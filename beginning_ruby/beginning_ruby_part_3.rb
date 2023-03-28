090e3c8f53052185d61e27df3567ea3366b762b8
Graham Jarvis <zeroThousand111@proton.me>
2023-03-28 11:03:46 +0000

    modify beginning_ruby_part_3.rb to only output array indexes above 4

--- beginning_ruby/beginning_ruby_part_3.rb
+++ beginning_ruby/beginning_ruby_part_3.rb
@@ -1,10 +1,14 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-index = 0
+index = 4
 loop do
     puts array[index]
     index +=1
     break if index == array.size
 end
 
-array.each { |item| puts item}
+array.each { |item| puts item if item >=5}
+
+array.each do |item|
+    puts item if item >= 5 
+end
