4fc774bb2b0ba907109350906cb6901ea39474ee
Graham Jarvis <zeroThousand111@proton.me>
2023-03-28 13:49:23 +0000

    moved beginning_ruby_part_3.rb to part_3 directory

---
+++ beginning_ruby/part_3/beginning_ruby_part_3.rb
@@ -0,0 +1,49 @@
+require "pry"
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# puts every item in the array using a loop
+# index = 4
+# loop do
+#     puts array[index]
+#     index +=1
+#     break if index == array.size
+# end
+
+# puts every item in the array using each method
+# array.each { |item| puts item if item >=5}
+
+# array.each do |item|
+#     puts item if item >= 5 
+# end
+
+# create a new array based on the select method
+# new_array = array.select do |item|
+#     item.odd?
+# end
+
+# create a new transformed array based on the map method
+# new_array = array.map do |item|
+#     item + 1
+# end
+
+# mutate the array using the map! method
+# array.map! do |item|
+#     item + 1
+# end
+
+# find the sum of all the numbers
+# index = 0
+# sum = 0
+# loop do
+#     sum += array[index]
+#     index +=1
+#     return sum if index == array.size
+#     binding.pry
+# end
+# p sum  
+
+sum = array.sum
+
+p sum
+# p new_array
+p array
