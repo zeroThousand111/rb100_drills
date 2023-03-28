928136bf0fe9209fccbb62373026eab04ffb17ed
Graham Jarvis <zeroThousand111@proton.me>
2023-03-28 11:32:52 +0000

    update to include map and map! methods on array

--- beginning_ruby/beginning_ruby_part_3.rb
+++ beginning_ruby/beginning_ruby_part_3.rb
@@ -1,5 +1,6 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# puts every item in the array using a loop
 # index = 4
 # loop do
 #     puts array[index]
@@ -7,14 +8,27 @@ array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 #     break if index == array.size
 # end
 
+# puts every item in the array using each method
 # array.each { |item| puts item if item >=5}
 
 # array.each do |item|
 #     puts item if item >= 5 
 # end
 
-new_array = array.select do |item|
-    item.odd?
+# create a new array based on the select method
+# new_array = array.select do |item|
+#     item.odd?
+# end
+
+# create a new transformed array based on the map method
+# new_array = array.map do |item|
+#     item + 1
+# end
+
+# mutate the array using the map! method
+array.map! do |item|
+    item + 1
 end
 
-p new_array
+# p new_array
+p array
