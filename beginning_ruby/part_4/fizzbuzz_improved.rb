a88cb1c192131e18f66f908c3726f8173edddcc1
Graham Jarvis <zeroThousand111@proton.me>
2023-03-30 03:57:15 +0000

    update fizzbuzz_improved.rb

--- beginning_ruby/part_4/fizzbuzz_improved.rb
+++ beginning_ruby/part_4/fizzbuzz_improved.rb
@@ -1,16 +1,35 @@
+# def fizzbuzz(start, finish)
+#     array = (start..finish).to_a
+#     array.each do | number|
+#         if (number % 3 == 0) && (number % 5 == 0)
+#             puts "FizzBuzz"
+#         elsif number % 3 == 0
+#             puts "Fizz"
+#         elsif number % 5 == 0 
+#             puts "Buzz"
+#         else 
+#             puts number
+#         end
+#     end
+# end
+
+
 def fizzbuzz(start, finish)
     array = (start..finish).to_a
-    array.each do | number|
+    new_array = []
+    new_array = array.map do | number|
         if (number % 3 == 0) && (number % 5 == 0)
-            puts "FizzBuzz"
+            "FizzBuzz"
         elsif number % 3 == 0
-            puts "Fizz"
+            "Fizz"
         elsif number % 5 == 0 
-            puts "Buzz"
+            "Buzz"
         else 
-            puts number
+            number
         end
     end
+    puts new_array.join(", ")
 end
+
 
 fizzbuzz(1,15)
